bd160fc1df6f8893703bbc0a34c5a4246ffd2b53
Cristian Merli <stesvis@gmail.com>
2017-10-11 16:32:23 -0600

    First commit

---
+++ Shared_2/Person_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shared_2
+{
+    public class Person_2
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ Shared_3/Person_3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shared_3
+{
+    public class Person_3
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
